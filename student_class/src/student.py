1f9f3b71ba6c9f2abe0666799aed655564772304
Panagiotis <tsamou7@gmail.com>
2023-04-10 21:04:37 +0100

    i added comments

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -1,13 +1,16 @@
 
 class Student():
 
+    #it initialises the class properties
     def __init__(self, input_studen_name, input_conhort):
         self.name = input_studen_name
         self.cohort = input_conhort
 
+    #it returns the string "I can talk!"
     def talk(self):
         return "I can talk!"
 
+    #it takes the student's fav language  and puts in a string
     def say_favourite_language(self, coding_language):
         result = f'I love {coding_language}'
         return result
--- team_class/src/team.py
+++ team_class/src/team.py
@@ -1,17 +1,24 @@
 class Team():
 
+        #initialise my class properties
     def __init__(self, input_team_name, input_players, input_coach) :
         self.name = input_team_name
         self.players = input_players
         self.coach = input_coach
         self.points = 0 
 
+
+    # adds a player in the list of team's players
     def add_player(self, new_player):
         self.players.append(new_player)
     
+
+    #it takes a name and checks if he is in the team
     def has_player(self, looking_for_player):
         return looking_for_player in self.players
 
+
+    #it adds or takes away 3 points if the team wins or loses respectively
     def play_game(self, win_lose):
         if not win_lose:
             if self.points==0:
